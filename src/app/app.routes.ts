2e54856d0e19cc2d6826afb8322940d7a6ad2b39
pratiksha <pratikshajankar176@gmail.com>
2024-06-11 18:40:02 +0530

    adding APIs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/Admin/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',redirectTo:'login',pathMatch:'full'
+    },
+    {
+        path:'login',component:LoginComponent
+    }
+];
--- src/app/core/constant/APIConstant.ts
+++ src/app/core/constant/APIConstant.ts
@@ -1,3 +1,47 @@
 export const APIConstant={
+    // Common url
+    API_URL:"https://freeapi.gerasim.in/api/BigBasket/",
+
+    // login and registration APIs
+    LOGIN:"Login",
+    REGISTRATION:"RegisterCustomer",
+    UPDATE_PROFILE:"UpdateProfile",
+
+    // customer APIs
+    GET_CUSTMER_BY_ID:"GetCustomerById?id=",
+    GET_ALL_CUSTOMER:"GetAllCustomer",
+
+    // Offer APIs
+    GET_ALL_OFFER:"GetAllOffers",
+    CREATE_OFFER:"CreateNewOffer",
+    
+    // category APIs
+    GET_ALL_CATEGORY:"GetAllCategory",
+    CREATE_CATEGORY:"CreateNewCategory",
+
+    // order APIs
+    PLACE_ORDER:"PlaceOrder",
+    CANCEL_ORDER:"cancelOrder?saleId=",
+
+    // Sale APIs
+    GET_ALL_SALE_BY_CUSTOMERID:"GetAllSaleByCustomerId?id=",
+    GET_SALE_BY_SALEID:"OpenSaleBySaleId?saleId=",
+    GET_ALL_SALE_ITEM:"GetAllSaleItems",
+
+    // Cart APIs
+    GET_CART_PRODUCT_BY_CUSTOMERID:"GetCartProductsByCustomerId?id=",
+    GET_ALL_CART_ITEM:"GetAllCartItems",
+    ADD_TO_CART:"AddToCart",
+    DELETE_PRODUCT_FROM_CARTID:"DeleteProductFromCartById?id=",
+
+    // Product APIs
+    GET_ALL_PRODUCT:"",
+    GET_PRODUCT_BY_CATEGORYID:"",
+    CREATE_PRODUCT:"",
+    UPDATE_PRODUCT:"",
+    DELETE_PRODUCT_BY_ID:"",
+    GET_PRODUCT_BY_ID:"",
+    GET_CART_PRODUCT_BY_CUSTID:"",
+    
 
 }
